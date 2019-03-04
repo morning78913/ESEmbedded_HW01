caa534f3a5e040c3ecd7a4047d025b75729fc6a7
morning78913 <morning78913@yahoo.com.tw>
2019-03-04 21:16:08 +0800

    Add Array of Function Pointer

--- main.c
+++ main.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int plus(int a, int b) { return a + b; }
-int minus(int a, int b) { return a - b; }
-int multiply(int a, int b) { return a * b; }
-int divided(int a, int b) { return a / b; }
-int main()
+int plus(int a, int b){return a + b;}
+int minus(int a, int b){return a - b;}
+int multiply(int a, int b){return a * b;}
+int divided(int a, int b){return a / b;}
+
+char operation[] =
 {
-	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+  '+',
+  '-',
+  '*',
+  '/'
+};
+
+int (*Calc[4])(int, int)  =
+{
+  plus,
+  minus,
+  multiply,
+  divided
+};
+
+int main(){
+ 
+ int a, c;
+ char b;
+ printf("Key a Function \nEx: 5 + 5\n");
+ scanf("%i %c %i", &a, &b, &c);
+
+ for(int i=0; i<4; i++)
+ {
+     if(b == operation[i])  printf("%d %c %d = %d",a ,b, c, (*Calc[i])(a, c));
+ }
+
+ //IF ELSE IF
+/* if(b == '+')   printf("%d %c %d = %d\n", a, b, c, Calc(a, c, plus));
+ else if(b == '-')  printf("%d %c %d = %d\n", a, b, c, Calc(a, c, minus));
+ else if(b == '*')  printf("%d %c %d = %d\n", a, b, c, Calc(a, c, multiply));
+ else if(b == '/')  printf("%d %c %d = %d\n", a, b, c, Calc(a, c, divided));
+*/ 
 }
+
+
